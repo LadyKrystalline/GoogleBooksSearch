d6900fb4efcfd534b4e806e0fde5025e3611aacb
Krystal Sayles <kmsayles@student.fullsail.edu>
2019-12-10 17:15:41 -0500

    ADD class with an Async Task to handle Network connectivity - from a code snippet I used in a test application (May need some more customization/adjustments as features are added)

---
+++ app/src/main/java/com/example/googlebookssearch/data/FetchBooks.java
@@ -0,0 +1,67 @@
+package com.example.googlebookssearch.data;
+
+import android.os.AsyncTask;
+
+import com.example.googlebookssearch.objects.Book;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FetchBooks extends AsyncTask<String, Void, String> {
+
+    public interface SearchListener{
+        void OnResult(ArrayList<Book> books);
+        void ShowMessage(String message);
+        boolean hasConnection();
+        File getCacheDirectory();
+    }
+
+    private final SearchListener listener;
+
+    public FetchBooks(SearchListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String queryString = strings[0];
+
+        File file = new File (this.listener.getCacheDirectory(), queryString + ".json");
+        if (!this.listener.hasConnection()){
+            String fileContents = DataUtils.loadFile(file);
+            if (fileContents != null){
+                this.listener.ShowMessage("You are offline. Showing cached data.");
+            } else {
+                this.listener.ShowMessage("No Internet Connection.");
+            }
+            return fileContents;
+        }
+
+
+        String response = DataUtils.loadData(queryString);
+        if (response != null){
+            DataUtils.saveFile(file, response);
+        } else {
+            this.listener.ShowMessage("An error occurred.");
+        }
+        return response;
+
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        if (s == null){
+            return;
+        }
+
+        ArrayList<Book> books = DataUtils.parseData(s);
+        if(books != null){
+            this.listener.OnResult(books);
+        } else {
+            this.listener.ShowMessage("An error occurred.");
+        }
+
+    }
+
+}
